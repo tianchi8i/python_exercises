d9b4a428b1ad47831ac2726becf869563844c144
unknown <tianchi@8i.com>
2022-02-23 15:05:29 +1300

    Removed the Week one folder from Week two branch

--- Python Tutorial Exercises(Week 1)/#3.py
+++
@@ -1,8 +0,0 @@
-#3
-a = "harry had a little lamb"
-b = "ha" 
-
-for b in a:
-    z = a.count(b)
-    print(z)
-    break
--- Python Tutorial Exercises(Week 1)/#4.py
+++
@@ -1,9 +0,0 @@
-#4
-store_products = ["fruit", "water", "bread", "toilet paper", "soap"]
-shopping_list = ["eggs", "tooth paste", "toilet paper", "ice cream", "water"]
-
-for x in shopping_list:
-    if x in store_products:
-        print(x)
-        #toilet paper,water
-        break
