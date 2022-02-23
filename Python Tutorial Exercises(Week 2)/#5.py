a7f39d01cf97409fbdc1395dd15855aa7c4b3ad9
unknown <tianchi@8i.com>
2022-02-23 16:05:45 +1300

    deleted Week two folder from Week three branch

--- Python Tutorial Exercises(Week 2)/#5.py
+++
@@ -1,7 +0,0 @@
-Ingredient_list = ["rice","cheese", "pasta", "tomato sauce", "broccoli"]
-for m in range(0, len(Ingredient_list)):
-
-    for u in range(m, len(Ingredient_list)):
-        if m == u:
-            continue
-        print(Ingredient_list[m], "and", Ingredient_list[u])
