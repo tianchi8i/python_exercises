21a0ac3b5aaea7b4b8769278f3a9856ebc7ffd32
unknown <tianchi@8i.com>
2022-03-18 00:24:19 +1300

    #3file bug fix

--- Python Tutorial Exercises(Week 3)/#3.py
+++ Python Tutorial Exercises(Week 3)/#3.py
@@ -8,15 +8,12 @@ the final amount he has saved once the goal has been reached.
 g = 1000
 week_number = 0
 total_number = 0
-s = input("Enter a number: ")
 
-while s != g:   
+while g > total_number :  
 
     #Add one to the week_number list/container, every time Andrew enter a number
     week_number += 1
     #Add the input number to the total list/container
-    total_number += int(s)
-    #Once the 'total number' is greater then 'g'($1000),print the message and break the loop
-    if int(total_number) > g: 
-        print(f"He has saved enough at week {week_number} with a total of $ {total_number}.")
-        break
+    total_number += int(input("Enter a number: "))
+#Once the 'total number' is greater then 'g'($1000),print the message
+print(f"He has saved enough at week {week_number} with a total of $ {total_number}.")
