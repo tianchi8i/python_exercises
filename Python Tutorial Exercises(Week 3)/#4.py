16cd2a3409f132b850b2f05d98e2f33c4c694c99
unknown <tianchi@8i.com>
2022-03-19 19:21:28 +1300

    bug fix

--- Python Tutorial Exercises(Week 3)/#4.py
+++ Python Tutorial Exercises(Week 3)/#4.py
@@ -11,22 +11,18 @@ by using a nested while loop.
 '''
 first_names  = ["Tianchi", "Ariel", "Patrick", "Andrew"]
 last_names = ["Jane", "Savill", "Yap", "Ma"]
+len_last_names = len(last_names)
 
-#for loop
-for v,b in zip(first_names,last_names[::-1]):
-    print(v,b)
+# for loop
+for first_name,last_name in zip(first_names,last_names[::-1]):
+    print(first_name,last_name)
 
 #Wile Loop
 first_names  = ["Tianchi", "Ariel", "Patrick", "Andrew"]
 last_names = ["Jane", "Savill", "Yap", "Ma"]
 
-x = len(last_names) - 1
-y = len(first_names)
-z = 0
-
-while z < y and x >= 0:
-
-    print(first_names[z],last_names[x])
-    
-    x -= 1
-    z += 1
+reversed_last_names = list(reversed(last_names))
+number = 0
+while number != len(first_names):
+        print(first_names[number], reversed_last_names[number])
+        number += 1
--- Python Tutorial Exercises(Week 3)/#7.py
+++ Python Tutorial Exercises(Week 3)/#7.py
@@ -3,25 +3,22 @@ Solve Week 2 exercises using while loops instead of for loops
 '''
 #1
 x = [["this", "is", "a", "nested"], ["list", "that"], ["will", "be", "printed", "out"]]
-def nest_list(x):
-    
-    new_list = []
-    z = 0
-    t = nest_list(x)
-    while z < len(x):
-        if isinstance(x[z], list):
-            new_list.extend(t[z]) 
-
-        else:
-            new_list.append(x[z])
-        z += 1
-
-    return new_list
-
-i = 0
-while i < len(t):
-    print(t[i])
-    i += 1
+#                0                         1                         2
+nested_list_one = 0
+nested_list_two = 0
+#len(x)=3 and nested_list_one will never greater than 3 becuse there only 3 list in the x list[0,1,2]. So we use '<' in here.
+while nested_list_one < len(x):
+    #nested_list_two = [ [0,1,2,3], [0,1], [0,1,2,3]  ] . 
+    #len(x[nested_list_one]) = 4444(nested list 1 have 4 items and so on),22,4444.
+    #nested_list_two will never greater than nested_list_one, so use '<' in here.
+    while nested_list_two < len(x[nested_list_one]):
+        print(x[nested_list_one][nested_list_two])
+        nested_list_two += 1
+        #reset loop [1,2,3,4] &[4,4,4,4] if 4 == 4 reset list two and break.
+        if nested_list_two == len(x[nested_list_one]):
+            nested_list_two = 0
+            break
+    nested_list_one += 1
 
 #2
 c = 6
