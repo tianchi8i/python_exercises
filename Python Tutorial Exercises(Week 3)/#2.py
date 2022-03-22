149644449eb60b76bc16898f3dca373469b09d43
unknown <tianchi@8i.com>
2022-03-22 15:15:59 +1300

    #2 file bug fix. Cant think of a better idea to fix it.

--- Python Tutorial Exercises(Week 3)/#2.py
+++ Python Tutorial Exercises(Week 3)/#2.py
@@ -19,58 +19,19 @@ for i in range(x,y+1):
         prime_number_list.append(i)
 print(prime_number_list)
 
-#while loop
-# set range
-while u >= x and u <= y:
-    count = 0
-    #q = 2 is used for checking the factor of the number
-    # we start dividing by 2. 
-    # This is because divide 0 is invalid and any number can be divided by 1.
-    # So we will make a variable with the starting value of 2.
-    q = 2
-    
-    #use a while loop to calculate the prime number. 
-    # The smallest prime number is 2. the largest factor for q must be 
-    # smaller and equal to u/2, and check all integers smaller or equal 
-    # or smaller and equal to u/2. 
-    while q <= u/2:
-        #We are dividing the number by all the numbers using u%q == 0
-        if u%q == 0:
-            count += 1      
-            #The break statement is used to come out of 
-            #the loop as soon we get any positive divisor then no further check is required.
-            break
-        q += 1
-    #check the count number
-    if count == 0:
-        prime_number_list.append(u)
-    u += 1
-#printing the prime numbers between x and y
-print(prime_number_list)
-
 Min = 20
 Max = 47
-prime_numbers = []
 i = Min
+prime_numbers = []
 
 while i >= Min and i <= Max:
-    q = 2
-
-    isPrime = False # for integer = 1
-    if i > 1: 
-        isPrime = True
-    
-    while q <= int(i/2):
-
-        if i%q == 0: 
-            isPrime = False 
+    j = 2
+    while j < i:
+        if i % j == 0:
             break
-        q += 1
-    else:
-        if isPrime:
-            prime_numbers.append(i)
-            if not isPrime:
-                print(i)
+        else:
+            if j+1 == i:
+                prime_numbers.append(i)
+        j += 1
     i += 1
-
 print(prime_numbers)
