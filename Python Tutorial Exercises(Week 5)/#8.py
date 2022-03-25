08ab9e70ca6d062a270df26c8b00be581027b524
unknown <tianchi@8i.com>
2022-03-25 18:59:17 +1300

    new update for #8 file

--- Python Tutorial Exercises(Week 5)/#8.py
+++ Python Tutorial Exercises(Week 5)/#8.py
@@ -39,17 +39,15 @@ print("\n".join([i * "6" for i in range(1, c + 1)]))
 # week2.3
 b = 4
 
-list = [((b - i) * "*" + str(i) + ((i - 1) * f"{i}")) for i in range(b,0,-1)]
-list.reverse()
-print("\n".join(list))
+print("\n".join([((b - i) * "* " + str(i) + ((i - 1) * f" {i}")) for i in range(b,0,-1)][::-1]))
 
 # week2.4
 y = [4, 8, 2, 12, 7, 10]
 word1 = "List has an odd number"
 word2 = "List does not have an odd number"
 
-list = [i if i % 2 != 0 else word1 for i in y]
-print("\n".join([i if i == word1 else word2 for i in list]))
+odd_number = [i if i % 2 != 0 else word1 for i in y]
+print("\n".join([i if i == word1 else word2 for i in odd_number]))
 
 
 # week 2.5
@@ -57,3 +55,26 @@ Ingredient_list = ["rice", "cheese", "pasta", "tomato sauce", "broccoli"]
 
 i = [str(f"{Ingredient_list[i]} and {Ingredient_list[n]}") for i in range(0, len(Ingredient_list)) for n in range(i, len(Ingredient_list)) if i != n]
 print("\n".join(i))
+
+# week 3.1
+n = 5
+factorial = 1
+factorial_list=[]
+print([factorial_list[-1] for x in range(1, n+1) if not factorial_list.append(x*factorial_list[-1] if factorial_list else 1)][-1])
+
+#week 3.2
+x = 20
+y = 47
+every_number = set([i for i in range(x, y + 1) for a in range(2, i) if i % a != 0])
+non_prime_numbers = set([i for i in range(x, y + 1) for a in range(2, i) if i % a == 0])
+prime_numbers = list(every_number - non_prime_numbers)
+prime_numbers.sort(reverse=False)
+print(prime_numbers)
+# week3.3
+#can't use while loop in list comprehension
+
+# week 3.4
+first_names  = ["Tianchi", "Ariel", "Patrick", "Andrew"]
+last_names = ["Jane", "Savill", "Yap", "Ma"]
+print("\n".join([' '.join(x) for x in zip(first_names,last_names[::-1])]))
+
