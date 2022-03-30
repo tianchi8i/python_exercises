834305810a41965e88eee3c8651cf37540a6d602
unknown <tianchi@8i.com>
2022-03-30 13:22:26 +1300

    new update for #4 file

--- Python Tutorial Exercises(Week 5)/#4.py
+++ Python Tutorial Exercises(Week 5)/#4.py
@@ -1,43 +1,44 @@
 '''
-use if len(i) >= 8 to find all the strings that have at least 8 characters or more.
+use of len(password) < 8 to find all the strings that are less than 8 characters (we don't want them so return False others go to the next filter)
 
-use if not sym.isalnum() to get the strings with least one character is not alphanumeric
+use password.isalnum()  to check if all characters in the string are alphanumeric or not. 
+it turns True if all characters in the string are alphanumeric (We wanted the strings with at least one Symbol so return False go to the next filter)
 
-use if sum(map(str.islower, low)) >= 1 to get the strings with least one character is lowercase letter
-use mapfunction you can specify the function then you specify the data to iterate over.
-the map function will return an iterator over the collection of str.islower applied to each 'low' value.
+use isalpha() if to check if all characters in the string are alphabetic or not. 
+it turns True if all characters in the string are alphabetic 
+(We wanted the strings with symbols and alphanumeric so return False go to the next filter)
 
-use if sum(map(str.isupper, up)) >= 1 to get the strings with least one character is uppercase letter
+use if password.islower(): to get the strings with least one character is uppercase letter.
+The islower() method returns True if all the characters are in lower case, otherwise False.
+(We dont know the password to be all the characters are in lower case, so return False go to the next filter)
+
+use if password.isupper(): to get the strings with least one character is uppercase letter.
+The isupper() method returns True if all the characters are in upper case, otherwise False.
+(We dont know the password to be all the characters are in upper case, so return False go to the next filter)
     
-use if sum(map(str.isdigit, num)) >= 2 to get the strings with least two character is numbers
+use if (sum(c.isdigit() for c in password) < 2): to check if the strings/password has at least 2 number turn True other False.
 
 '''
+def check_password(password):
+    if len(password) < 8:
+        return False
 
-def check_password(andrews_passwords, num):
-    if num == 'characters':
-        characters = [char for char in andrews_passwords if len(char) >= 8]
-        return characters
-    elif num == 'symbol':
-        symbol = [sym for sym in andrews_passwords if not sym.isalnum()]
-        return symbol
-    elif num == 'lowercase':
-        lowercase = [low for low in andrews_passwords if sum(map(str.islower, low)) >= 1]
-        return lowercase
-    elif num == 'uppercase':
-        uppercase = [up for up in andrews_passwords if sum(map(str.isupper,up)) >= 1]
-        return uppercase
-    elif num == 'number':
-        number = [num for num in andrews_passwords if sum(map(str.isdigit, num)) >= 2 ]
-        return number
-    elif num == 'strong_passward':
-        strong_passward = [num for num in [up for up in [low for low in [sym for sym in [char for char in andrews_passwords if len(char) >= 8] if not sym.isalnum()] if sum(map(str.islower, low)) >= 1] if sum(map(str.isupper,up)) >= 1] if sum(map(str.isdigit, num)) >= 2 ]
-        return strong_passward
-    
+    if password.isalnum():
+        return False
+
+    if not any(c.isalpha() for c in password):
+        return False
+
+    if password.islower():
+        return False
+
+    if password.isupper():
+        return False
+
+    if (sum(c.isdigit() for c in password) < 2):
+        return False
+
+    return True
+
 andrews_passwords = ["123456", "hAhA123!", "MyCarR0cks*", "EasyPassword123$$", "AnotherPassword??", "letmein"]
-
-print(check_password(andrews_passwords, 'characters'))
-print(check_password(andrews_passwords, 'symbol'))
-print(check_password(andrews_passwords, 'lowercase'))
-print(check_password(andrews_passwords, 'characters'))
-print(check_password(andrews_passwords, 'number'))
-print(check_password(andrews_passwords, 'strong_passward'))
+print([password for password in andrews_passwords if check_password(password)])
